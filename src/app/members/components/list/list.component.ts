fecb6c94a69514a24f1be4a51f9596bd9a8a96d5
EgorKirillov <80296253508@mail.ru>
2022-11-11 15:24:54 +0300

    fix: list component

--- src/app/members/components/list/list.component.ts
+++ src/app/members/components/list/list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Params } from '@angular/router'
+import { Observable } from 'rxjs'
+import { FinancialType, Members } from '../../models/members'
+import { MembersService } from '../../services/members.service'
+import { tabsNames } from '../../../data/tabs'
 
 @Component({
   selector: 'ts-list',
@@ -7,13 +11,18 @@ import { ActivatedRoute, Params } from '@angular/router'
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
-
+  constructor(private route: ActivatedRoute, private membersService: MembersService) {}
   queryParam: Params | undefined
+
+  members$!: Observable<Members[]>
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((param: Params) => {
+      let sortByType: FinancialType = 'income'
       this.queryParam = param
+      sortByType = tabsNames[param['tab']]
+      this.membersService.getFilteredMembers(sortByType)
     })
+    this.members$ = this.membersService.filteredMembers$
   }
 }
