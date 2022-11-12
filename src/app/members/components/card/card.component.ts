fae3b385a1db2c7c8116eb2c48f6754d4540b2b3
EgorKirillov <80296253508@mail.ru>
2022-11-12 13:33:42 +0300

    refactor:  clear component

--- src/app/members/components/card/card.component.ts
+++ src/app/members/components/card/card.component.ts
@@ -1,29 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { FinancialType } from '../../models/members'
-import { MembersService } from '../../services/members.service'
-import { Observable } from 'rxjs'
-import { Router } from '@angular/router'
 
 @Component({
   selector: 'ts-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit {
-  constructor(private membersService: MembersService, private router: Router) {}
-
+export class CardComponent {
   @Input() type!: FinancialType
+  @Input() count!: number | null
   @Input() index!: number
-
-  count$!: Observable<number>
-  count: number | undefined
-  clickButtonHandler() {
-    this.router.navigate(['navigator'], { queryParams: { tab: this.index } })
-  }
-
-  ngOnInit(): void {
-    this.membersService.getFilteredMembersCount(this.type)
-    this.count$ = this.membersService.filteredMembersLenght$
-    this.count = this.membersService.filteredMembersLenght
-  }
 }
