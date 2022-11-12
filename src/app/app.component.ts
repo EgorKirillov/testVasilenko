e052eebc0fc3e5fe17c97e4c4678d8cbb7afff4a
EgorKirillov <80296253508@mail.ru>
2022-11-12 13:40:39 +0300

    refactor: tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { MembersService } from './shared/services/members.service'
 
 @Component({
   selector: 'ts-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test01'
+  constructor(private membersService: MembersService) {}
+
+  ngOnInit(): void {
+    this.membersService.getMembers()
+  }
 }
--- src/app/members/components/tabs/tabs.component.ts
+++ src/app/members/components/tabs/tabs.component.ts
@@ -1,24 +1,20 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 import { FinancialType } from '../../models/members'
 import { tabsNames } from '../../../data/tabs'
 import { ActivatedRoute } from '@angular/router'
+import { map, Observable } from 'rxjs'
 
 @Component({
   selector: 'ts-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss'],
 })
-export class TabsComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
-
+export class TabsComponent {
   tabsNames: FinancialType[] = tabsNames
   currentLink: number = 0
+  currentTabIndex: Observable<string>
 
-  changeCurrent(i: number) {
-    this.currentLink = i
-  }
-
-  ngOnInit(): void {
-    this.currentLink = Number(this.route.snapshot.queryParamMap.get('tab'))
+  constructor(private route: ActivatedRoute) {
+    this.currentTabIndex = route.queryParams.pipe(map(param => param['tab']))
   }
 }
