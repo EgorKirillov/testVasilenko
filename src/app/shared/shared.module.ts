fd84069362d32db531a610660272132da1a17fb8
EgorKirillov <80296253508@mail.ru>
2022-11-12 13:44:11 +0300

    refactor: delete unused components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { TabsComponent } from './tabs/tabs.component'
-import { CardComponent } from './card/card.component'
-import { ListElementComponent } from './list-element/list-element.component'
 
 @NgModule({
-  declarations: [TabsComponent, CardComponent, ListElementComponent],
-  exports: [TabsComponent, CardComponent],
+  declarations: [],
+  exports: [],
   imports: [CommonModule],
 })
 export class SharedModule {}
