54cdd7585918faea4f645e78a9d17ab0e453f8c2
EgorKirillov <80296253508@mail.ru>
2022-11-12 13:38:31 +0300

    refactor: move service

--- src/app/members/components/summary/summary.component.ts
+++ src/app/members/components/summary/summary.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core'
-import { MembersService } from '../../services/members.service'
-import { FinancialType } from '../../models/members'
+import { Component } from '@angular/core'
+import { MembersService } from '../../../shared/services/members.service'
+import { FinancialType, Members } from '../../models/members'
 import { tabsNames } from 'src/app/data/tabs'
+import { map, Observable } from 'rxjs'
 
 @Component({
   selector: 'ts-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss'],
 })
-export class SummaryComponent implements OnInit {
-  constructor(private membersService: MembersService) {}
-
-  totalCount: number | null = null
+export class SummaryComponent {
   tabsNames: FinancialType[] = tabsNames
+  members$: Observable<Members[]>
+  filteredMembersCounts: Observable<number>[]
 
-  ngOnInit(): void {
-    this.totalCount = this.membersService.getTotalCount()
+  constructor(private membersService: MembersService) {
+    this.members$ = this.membersService.members$
+    this.filteredMembersCounts = tabsNames.map(type => this.getFilteredMembersObservable(type))
+  }
+
+  private getFilteredMembersObservable(type: FinancialType): Observable<number> {
+    return this.members$.pipe(map(members => members.filter(member => member.type === type).length))
   }
 }
